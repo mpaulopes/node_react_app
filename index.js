5666dae5a59ca927c864d82bb3ed9af1b0667b6f
Marcus Lopes <marcus.p.lopes@gmail.com>
2019-05-02 17:24:53 -0700

    save user + promisse

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
-const express = require("express");
+const express = require('express');
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
+require('./models/User');
+require('./services/passport');
+
+mongoose.connect(keys.mongoURI);
+
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send({ hi: "There" });
-});
+//this syntax bellow is used when there is no need to create a instantiated varible. it's a single case as opposed to => const authRoutes = require('./routes/authRoutes')
+
+require('./routes/authRoutes')(app); 
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
+
